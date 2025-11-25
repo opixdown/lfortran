2846608eaa55ca10d800e12a7cb369a14204b73d
opixdown <atharvawaghmare34@gmail.com>
2025-11-26 00:46:50 +0530

    Updated the test case

--- test.f90
+++ test.f90
@@ -1,3 +1,20 @@
+module types_m
+  implicit none
+
+  type int_t
+    integer :: i
+  end type
+
+  type(int_t), parameter ::  &
+    T_ONE = int_t(1),  &
+    T_TWO = int_t(2),  &
+    T_THREE = int_t(3)
+
+  type(int_t), parameter ::  &
+    T_ALL(3) = (/ T_ONE, T_TWO, T_THREE /)
+
+end module types_m
+
 program test
   use types_m
   implicit none
