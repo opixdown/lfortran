2dbaba7e816c19c115cac67fa6cf4c83e70365d3
opixdown <atharvawaghmare34@gmail.com>
2026-06-04 13:59:09 +0530

    add test

--- integration_tests/empty_array_01.f90
+++ integration_tests/empty_array_01.f90
@@ -1,10 +1,16 @@
 program empty_array_01
   integer, parameter :: x(0,0) = 0
+  integer, parameter :: z1(0) = 0
+  integer, parameter :: z3(0,0,0) = 0
   integer :: y(0,0)
   y = matmul(x, transpose(x))
   if (size(x) /= 0) error stop
   if (size(y) /= 0) error stop
   if (size(x,1) /= 0) error stop
   if (size(x,2) /= 0) error stop
+  if (iall(z1) /= not(int(0, kind(z1)))) error stop
+  if (iall(z3) /= not(int(0, kind(z3)))) error stop
+  if (iparity(z1) /= 0) error stop
+  if (iparity(z3) /= 0) error stop
   print *, "ok"
 end
